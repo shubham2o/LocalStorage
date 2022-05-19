d9b6bf9dc0cf22396edbe040deedbccee5755a66
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-19 18:04:58 +0530

    HTML DOM Element matches() added

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -16,3 +16,24 @@ document.getElementById("myCheckbox").addEventListener("click", function (event)
 function myReset1() {
     document.getElementById("myForm").reset();
 }
+
+
+// HTML DOM Element matches()
+// EXAMPLE 1 ||
+const element = document.getElementById("demo");
+let text;
+if (element.matches(".container")) {
+    text = "This element matches the CSS selector";
+} else {
+    text = "This element does not match the CSS selector";
+}
+document.getElementById("demo").innerHTML = text;
+
+
+// EXAMPLE 2 ||
+var element2 = document.getElementById("myElement");
+if (element2.matches(".container, .wrapper")) {
+    element2.innerHTML = "This element matches either the \".container\" CSS selector or the \".wrapper\" selector.";
+} else {
+    element2.innerHTML = "This element does not match any of the selectors.";
+}
