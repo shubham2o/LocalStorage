25772f3eb6ca426acb3a2fa74425bee9a63cdc8a
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-19 17:32:10 +0530

    preventDefault() Event Method added

---
+++ Doubts/doubts.js
@@ -0,0 +1,11 @@
+// preventDefault() Event Method
+// EXAMPLE 1 ||
+document.getElementById("myAnchor").addEventListener("click", function (event) {
+    event.preventDefault();
+});
+
+
+// EXAMPLE 2 ||
+document.getElementById("myCheckbox").addEventListener("click", function (event) {
+    event.preventDefault();
+});
