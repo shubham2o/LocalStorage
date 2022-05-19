c0447f9fa57da33d22557d7fba333a3bc0d72f4f
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-19 17:47:05 +0530

    Form reset() Method added

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -9,3 +9,10 @@ document.getElementById("myAnchor").addEventListener("click", function (event) {
 document.getElementById("myCheckbox").addEventListener("click", function (event) {
     event.preventDefault();
 });
+
+
+// Form reset() Method
+// EXAMPLE 1 ||
+function myReset1() {
+    document.getElementById("myForm").reset();
+}
